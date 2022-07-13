dbd04819312f0d54c788f97859254de2b7c83c73
Uszczi <mateuszpapuga24@gmail.com>
2022-07-13 20:54:59 +0200

    :hammer: fix

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 
+
 from django.http.response import HttpResponse
+from polls.models import Image
+
+from rich import inspect
+from django import forms
+
+
+class ImageForm(forms.ModelForm):
+    """Form for the image model"""
+
+    class Meta:
+        model = Image
+        fields = ("title", "image")
+
 
 def image_upload_view(request):
-    """Process images uploaded by users"""
-    print(request.FILES)
+    inspect(request)
+    Image(title=request.POST["title"], image=request.FILES["image"]).save()
+
     return HttpResponse()
-
-
